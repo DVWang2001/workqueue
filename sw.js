f88cb3b2400c6d303cb40366ec5827608e791556
David <bulawaloman@gmail.com>
2026-06-07 21:53:43 +0800

    fix: add interactive-widget=resizes-content to stop Android Chrome window scroll on keyboard open

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workqueue-v11';
+const CACHE = 'workqueue-v12';
 const SHELL = ['./', './index.html', './style.css', './app.js',
                './firebase-config.js', './manifest.json', './icon.svg'];
 
